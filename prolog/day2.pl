9f1727626e43937110bcdc3afeb65b32ac5051fd
Andrew Bullen <andrew@identified.com>
2012-12-29 22:03:53 -0800

    fix a couple things in day2.pl

--- prolog/day2.pl
+++ prolog/day2.pl
@@ -1,6 +1,7 @@
 % reverse the elements of a list
 myReverse([],[]).
-myReverse([Head|Tail],Reverse) :- reverse(Tail, TailReverse), append(TailReverse,[Head],Reverse).
+myReverse([Head|Tail],Reverse) :- append(TailReverse,[Head],Reverse),
+                                  myReverse(Tail, TailReverse).
 
 
 % myReverse([1,2,3],[3,2,1]).
@@ -9,8 +10,10 @@ myReverse([Head|Tail],Reverse) :- reverse(Tail, TailReverse), append(TailReverse
 % find the smallest element of a list
 
 smallest([Head|[]],Head).
-smallest([Head|Tail],Head) :- smallest(Tail,TailSmallest), TailSmallest >= Head.
-smallest([Head|Tail],TailSmallest) :- smallest(Tail, TailSmallest), TailSmallest =< Head.
+smallest([Head|Tail],Head) :- smallest(Tail,TailSmallest),
+                              TailSmallest >= Head.
+smallest([Head|Tail],TailSmallest) :- smallest(Tail, TailSmallest),
+                                      TailSmallest =< Head.
 
 % smallest([1,2,3], 1).
 % smallest([6,4,3], 3).
@@ -23,8 +26,8 @@ mySort(Unsorted,Sorted) :- smallest(Unsorted,Smallest),
                            append(Part1,[Smallest], Part1S),
                            append(Part1S,Part2,Unsorted),
                            append(Part1,Part2,UnsortedRest),
-                           mySort(UnsortedRest,SortedRest),
-                           append([Smallest],SortedRest,Sorted).
+                           append([Smallest],SortedRest,Sorted),
+                           mySort(UnsortedRest,SortedRest).
 
 % mySort([2,1,3], What).
 % mySort([1,6,3,8,4,9,3,7,6,4,5,9,0,12], What).
